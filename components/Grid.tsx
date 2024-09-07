62b31ca8b3b87efdfe3b11b22260a33b69df34e6
javidev <jventajas.hdez@gmail.com>
2024-09-07 16:52:19 +0100

    Create grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,13 +6,18 @@ const Grid = () => {
     return (
         <section id={"about"}>
             <BentoGrid className={"w-full py-20"}>
-                {gridItems.map((item, i) => (
+                {gridItems.map(({id, title, description, className,
+                                    img, imgClassName, titleClassName, spareImg}, i) => (
                     <BentoGridItem
-                        id={item.id}
+                        id={id}
                         key={i}
-                        title={item.title}
-                        description={item.description}
-                        className={item.className}
+                        title={title}
+                        description={description}
+                        className={className}
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}
                     />
                 ))}
             </BentoGrid>
